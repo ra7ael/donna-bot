ef7dba14349ce5467d5c1bf2472b0ec20b02a97b
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-09-15 13:43:04 -0300

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const webhook = require('./src/bot/webhook');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/webhook', webhook);
+
+app.get('/', (req, res) => {
+  res.send('🚀 Donna bot rodando!');
+});
+
+app.listen(PORT, () => {
+  console.log(Servidor rodando na porta ${PORT});
+});
