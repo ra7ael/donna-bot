00e9060427455e5cda706f4fe3051aca52ac6d5a
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-09-15 14:34:36 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const webhook = require('./bot/webhook');
+const webhook = require("./bot/webhook");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(bodyParser.json());
-app.use('/webhook', webhook);
 
-app.get('/', (req, res) => {
-  res.send('🚀 Donna bot rodando!');
+// Rotas
+app.use("/webhook", webhook);
+
+// Rota de teste
+app.get("/", (req, res) => {
+  res.send("🚀 Donna bot rodando!");
 });
 
+// Inicia o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
