44c3f654312d7e65152513c865b90fab52362505
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-09-21 08:10:44 -0300

    Update Reminder.js

--- src/models/Reminder.js
+++ src/models/Reminder.js
@@ -1,13 +1,10 @@
-// src/models/Reminder.js
 import mongoose from "mongoose";
 
-const ReminderSchema = new mongoose.Schema({
-  from: { type: String, required: true },
-  text: { type: String, required: true },
-  date: { type: Date, required: true },
+const reminderSchema = new mongoose.Schema({
+  from: { type: String, required: true },     // número do usuário
+  text: { type: String, required: true },     // mensagem do lembrete
+  date: { type: Date, required: true },       // quando deve disparar
   createdAt: { type: Date, default: Date.now }
 });
 
-const Reminder = mongoose.model("Reminder", ReminderSchema);
-
-export default Reminder;
+export default mongoose.model("Reminder", reminderSchema);
