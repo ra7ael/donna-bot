a5708dd1b1cad1f0125c92136ff66f3309f5ff48
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-09-30 16:34:33 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import OpenAI from "openai";
 import { MongoClient } from 'mongodb';
 import pdfParse from "pdf-parse/lib/pdf-parse.js";
+import bodyParser from "body-parser";
 import axios from 'axios';
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
