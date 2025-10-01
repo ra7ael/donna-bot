f4704f1dfbb83b3579dc50be696acd90bf233bd5
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-10-01 13:51:53 -0300

    Update autorizados.js

--- src/config/autorizados.js
+++ src/config/autorizados.js
@@ -1,6 +1,6 @@
 // Números que podem acessar TODAS as funções da Donna
 export const numerosAutorizados = [
   "554195194485", // seu número
-  "554196820681"// Carol
+  "554196820681",// Carol
   "554199833283"// trabalho
 ];
