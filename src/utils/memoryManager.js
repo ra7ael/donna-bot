dce089f22202f8ba7731f8e4b802c92780349fd8
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-10-02 10:01:16 -0300

    Update memoryManager.js

--- src/utils/memoryManager.js
+++ src/utils/memoryManager.js
@@ -1,5 +1,10 @@
 import Memoria from "../models/Memoria.js";
 
+/**
+ * Salvar dados na memória estruturada do usuário
+ * @param {String} userId
+ * @param {Object} dados - dados a serem armazenados (ex.: { nome, empresa, papeis })
+ */
 export async function salvarMemoria(userId, dados) {
   let memoria = await Memoria.findOne({ userId });
 
@@ -10,9 +15,24 @@ export async function salvarMemoria(userId, dados) {
   }
 
   await memoria.save();
+  console.log(`💾 Memória estruturada atualizada para ${userId}`);
   return memoria;
 }
 
+/**
+ * Buscar memória estruturada do usuário
+ * @param {String} userId
+ * @returns {Object|null} - memória armazenada
+ */
 export async function buscarMemoria(userId) {
   return await Memoria.findOne({ userId });
+}
+
+/**
+ * Apagar memória do usuário
+ * @param {String} userId
+ */
+export async function limparMemoria(userId) {
+  await Memoria.deleteOne({ userId });
+  console.log(`🗑️ Memória do usuário ${userId} apagada`);
 }
