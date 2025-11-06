0969a737e90e02f32717e2b08dc0f792a84634ac
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-11-06 18:09:53 -0300

    Update Memoria.js

--- src/models/Memoria.js
+++ src/models/Memoria.js
@@ -1,10 +1,1 @@
-// src/models/Memoria.js
-const memory = new Map();
 
-export function findInMemory(userMessage) {
-  return memory.get(userMessage.toLowerCase()) || null;
-}
-
-export function learnMemory(userMessage, answer) {
-  memory.set(userMessage.toLowerCase(), { answer });
-}
