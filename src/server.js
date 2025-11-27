1f874f0acd59598cd59adbbb21c28dd03e9c1670
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-11-27 17:26:25 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ import { buscarPergunta } from "./utils/buscarPdf.js";
 import multer from "multer";
 import { funcoesExtras } from "./utils/funcoesExtras.js";
 import { extractAutoMemoryGPT } from "./utils/autoMemoryGPT.js";
-import { querySemanticMemory } from "./models/semanticMemory.js";
+import { querySemanticMemory } from "./utils/memory.js";
 
 dotenv.config();
 
