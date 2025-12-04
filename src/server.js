abb99cd0622e7aeb8b84a805d6cccebffcadeb45
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-12-04 13:20:43 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ import { funcoesExtras } from "./utils/funcoesExtras.js";
 import { extractAutoMemoryGPT } from "./utils/autoMemoryGPT.js";
 import { addSemanticMemory, querySemanticMemory } from "./models/semanticMemory.js";
 import MemoriaEstruturada from "./models/memory.js";
-
+import { enqueueSemanticMemory } from "./utils/semanticQueue.js";
 mongoose.set("bufferTimeoutMS", 90000); // ⬆️ aumenta o tempo antes do timeout
 
 dotenv.config();
