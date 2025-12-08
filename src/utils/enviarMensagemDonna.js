37734b4735564d03d62077306bd00550f1da3559
ra7ael <105468059+ra7ael@users.noreply.github.com>
2025-12-08 09:30:49 -0300

    Create enviarMensagemDonna.js

---
+++ src/utils/enviarMensagemDonna.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import fs from "fs";
+
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
+
+export async function enviarMensagemDonna(to, { document, caption }) {
+  try {
+    const fileData = fs.readFileSync(document);
+
+    const form = new FormData();
+    form.append("messaging_product", "whatsapp");
+    form.append("to", to);
+    form.append("type", "document");
+    form.append("document", fileData, {
+      filename: document.split("/").pop(),
+      contentType: "text/plain"
+    });
+    form.append("caption", caption || "");
+
+    const url = `https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages`;
+
+    await axios.post(url, form, {
+      headers: {
+        Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+        ...form.getHeaders()
+      }
+    });
+
+    console.log("📄 Documento enviado com sucesso:", document);
+  } catch (err) {
+    console.error("❌ Erro ao enviar documento:", err.response?.data || err);
+  }
+}
